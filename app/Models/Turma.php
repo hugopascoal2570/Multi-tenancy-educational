ecb149a7dc2bfc4677084db012734b586ea09b25
Hugo Pascoal <hugo_pascoal_@hotmail.com>
2022-06-22 09:56:47 -0300

    fixed the search for teachers

--- app/Models/Turma.php
+++ app/Models/Turma.php
@@ -32,23 +32,12 @@ class Turma extends Model
     {
         return $query->where('tenant_id', auth()->user()->tenant_id);
     }
-    public function scopeRoleTeacher(Builder $query)
-    {
-        return $query->whereHas('roles', function($q){
-            $q->where('name', 'LIKE','%'.'Professor(a)'.'%');
-            $q->where('tenant_id', auth()->user()->tenant_id);
-        });
-    }
 
     public function teachersAvailable($filter = null)
     {
-        $teachers = User::whereNotIn('users.id', function ($query) {
-            $query->select('roles');
-            $query->where('name', 'LIKE','%'.'Professor(a)'.'%');
-            $query->where('tenant_id', auth()->user()->tenant_id);
-            $query->select('teacher_turma.turma_id');
-            $query->from('teacher_turma');
-            $query->whereRaw("teacher_turma.user_id={$this->id}");
+        $teachers = User::whereHas('roles', function($q){
+            $q->where('name', 'LIKE','%'.'Professor(a)'.'%');
+            $q->where('tenant_id', auth()->user()->tenant_id);
            
         })
             ->where(function ($queryFilter) use ($filter) {
